6a4c4ca775a99cb03b150ba81d157a79b84c8b31
Zahra <namazi.q@gmail.com>
2021-12-29 18:26:59 +0330

    fix bug

--- src/components/common/CountDown.jsx
+++ src/components/common/CountDown.jsx
@@ -4,35 +4,32 @@ import { diffTimeFormat } from '../../utils/time'
 
 const CountDown = (props) => {
   const { date, setLock } = props
-  const [waiting, setWaiting] = useState()
-
   // TODO change after Mr.teimori change mongodb
-  if (date) {
-    const [timer, setTimer] = useState(moment(date).diff(moment()) + 60000)
-    useEffect(() => {
-      let dif = moment.duration(timer)
-      let waitingTime = diffTimeFormat(dif)
-      setWaiting(waitingTime)
 
-      const interval = setInterval(() => {
-        if (timer < 1000) {
-          setLock(0)
-          clearInterval(interval)
-        } else {
-          let newTime = timer - 1000
-          setTimer(newTime)
-          let dif = moment.duration(newTime)
-          let waitingTime = diffTimeFormat(dif)
-          setWaiting(waitingTime)
-        }
-      }, 1000)
+  const [timer, setTimer] = useState(moment(date).diff(moment()))
+  const [waiting, setWaiting] = useState()
+  useEffect(() => {
+    let dif = moment.duration(timer)
+    let waitingTime = diffTimeFormat(dif)
+    setWaiting(waitingTime)
 
-      return () => {
+    const interval = setInterval(() => {
+      if (timer < 1000) {
+        setLock(0)
         clearInterval(interval)
+      } else {
+        let newTime = timer - 1000
+        setTimer(newTime)
+        let dif = moment.duration(newTime)
+        let waitingTime = diffTimeFormat(dif)
+        setWaiting(waitingTime)
       }
-    }, [timer])
-  }
+    }, 1000)
 
+    return () => {
+      clearInterval(interval)
+    }
+  }, [timer])
   return <div>{waiting}</div>
 }
 
